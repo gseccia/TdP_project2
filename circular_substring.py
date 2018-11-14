72bb513913a2f70753a1623a8472b5f7396abde0
gseccia <giuseppeseccia1997@gmail.com>
2018-11-14 20:14:16 +0100

    added tdp_collections and modified circular_substring.py.

--- circular_substring.py
+++ circular_substring.py
@@ -1,51 +1,10 @@
-def circular_substring(P, T):
-    n, m = len(T), len(P)
-    if m == 0:
-        return 0
-    fail = compute_kmp_fail(P)
-    j = 0
-    k = 0
-    while j < n + m - 1:
-        if j >= n:
-            new_j = j - n
-            if T[new_j] == P[k]:
-                if k == m - 1:
-                    return True
-                k += 1
-                j += 1
-
-            elif k > 0:
-                k = fail[k - 1]
-            else:
-                j += 1
-
-        elif T[j] == P[k]:
-            if k == m - 1:
-                return True
-            j += 1
-            k += 1
-        elif k > 0:
-            k = fail[k - 1]
-        else:
-            j += 1
-    return False
+from TdP_collections.text.find_kmp import find_kmp
 
 
-def compute_kmp_fail(P):
-    m = len(P)
-    fail = [0] * m
-    j = 1
-    k = 0
-    while j < m:
-        if P[j] == P[k]:
-            fail[j] = k + 1
-            j += 1
-            k += 1
-        elif k > 0:
-            k = fail[k - 1]
-        else:
-            j += 1
-    return fail
+def circular_substring(p, t):
+    m = len(p)
+    t += t[0:m - 1]
+    return True if find_kmp(t, p) >= 0 else False
 
 
 print(circular_substring("ingco", "commostring"))
