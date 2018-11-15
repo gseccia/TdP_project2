32b62e3fde1ba16d992d9b6f73e3e070776d5722
gseccia <giuseppeseccia1997@gmail.com>
2018-11-15 16:51:43 +0100

    divided in packages.

--- package_1/new_avl_tree_map.py
+++ package_1/new_avl_tree_map.py
@@ -4,29 +4,25 @@ from TdP_collections.map.binary_search_tree import TreeMap
 class NewAVLTreeMap(TreeMap):
     # -------------------------- nested _Node class --------------------------
     class _Node(TreeMap._Node):
-        """Node class for AVL maintains height value for balancing.
-
-        We use convention that a "None" child has height 0, thus a leaf has height 1.
-        """
         __slots__ = '_balance_factor'  # additional data member to store height
 
         def __init__(self, element, parent=None, left=None, right=None):
             super().__init__(element, parent, left, right)
             self._balance_factor = 0
 
-        def left_height(self):
-            pass
+        def get_balance_factor(self):
+            return self._balance_factor
 
-        def right_height(self):
-            pass
+        def set_balance_factor(self, bf):
+            self._balance_factor = bf
 
     # ------------------------- positional-based utility methods -------------------------
-
-    def _recompute_height(self, p):
+    def _recompute_balance_factor(self):
         pass
 
     def _isbalanced(self, p):
-        pass
+        """ ok """
+        return abs(p._node.get_balance_factor()) <= 1
 
     def _tall_child(self, p, favorleft=False):
         pass
@@ -37,8 +33,10 @@ class NewAVLTreeMap(TreeMap):
     def _rebalance(self, p):
         pass
 
+
+    # ---------------------------- override balancing hooks ----------------------------
     def _rebalance_insert(self, p):
-        pass
+        self._rebalance(p)
 
     def _rebalance_delete(self, p):
-        pass
+        self._rebalance(p)
