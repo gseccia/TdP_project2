a8dc1ab68af0225c31ae44a3edd0cf9708503d7c
gseccia <giuseppeseccia1997@gmail.com>
2018-11-20 10:25:39 +0100

    nope

--- tests/test_findRepetition.py
+++ tests/test_findRepetition.py
@@ -10,15 +10,24 @@ class TestFindRepetition(unittest.TestCase):
         self._testCases = []
 
     def testFindRepetition_trueResults(self):
-        self._testCases.append('./copy/test_copy_1')
-        self._testCases.append('./copy/test_copy_2')
+        expected = []  # expected tests result
 
-        for path in self._testCases:
-            self.assertTrue(find_repetition(path))
+        self._testCases.append('./copy/test_copy_1')
+        expected.append([['./copy/test_copy_1/copyDir.py', './copy/test_copy_1/copyDir2.py']])
+        self._testCases.append('./copy/test_copy_2')
+        expected.append([])
+
+        for i in range(len(self._testCases)):
+            self.assertEqual(expected[i], find_repetition(self._testCases[i]))
+
 
     def testFindRepetition_falseResults(self):
-        self._testCases.append('./copy/test_copy_3')
-        self._testCases.append('./copy/test_copy_4')
+        expected = []  # expected tests result
 
-        for path in self._testCases:
-            self.assertFalse(find_repetition(path))
+        self._testCases.append('./copy/test_copy_3')
+        expected.append()
+        self._testCases.append('./copy/test_copy_4')
+        expected.append()
+
+        for i in range(len(self._testCases)):
+            self.assertEqual(expected[i],find_repetition(self._testCases[i]))
